63c232a77e367c83e8302fc45cd0602372e5c246
Inês Justo <ines.zita7@gmail.com>
2019-12-21 17:24:09 +0000

    input values of hsv

--- main.cpp
+++ main.cpp
@@ -47,7 +47,11 @@ void printImageFeatures( const cv::Mat &image )
     std::cout << std::endl;
 }
 
-void filterImage(cv::Mat originalImage){
+int H_MIN; int H_MAX; int S_MIN; int S_MAX; int V_MIN; int V_MAX;
+cv::Mat originalImage;
+int CHOICE; int PREVCHOICE; int c; int d; int u; int num;
+
+void filterImage(){
     // Learning from https://www.youtube.com/watch?v=bSeFrPrqZ2A
     // object tracking... the easy way
     //      -> color filtering
@@ -62,7 +66,6 @@ void filterImage(cv::Mat originalImage){
     //          (blue, green, red) -> (hue, saturation, value)
     //
     //initial min and max HSV filter values. these will be changed using trackbars
-    int H_MIN = 0; int H_MAX = 256; int S_MIN = 0; int S_MAX = 256; int V_MIN = 0; int V_MAX = 256;
     //matrix storage for HSV image
     cv::Mat HSV;
     //matrix storage for binary threshold image
@@ -74,7 +77,7 @@ void filterImage(cv::Mat originalImage){
     // set 2:
     //      -> filter the colors of interest between a min and max threshold
 
-    H_MIN = 0; H_MAX = 495; S_MIN = 150; S_MAX = 491; V_MIN = 210; V_MAX = 400;
+    //H_MIN = 0; H_MAX = 495; S_MIN = 150; S_MAX = 491; V_MIN = 210; V_MAX = 400;
     cv::inRange(HSV, cv::Scalar(H_MIN,S_MIN,V_MIN), cv::Scalar(H_MAX,S_MAX,V_MAX), threshold);
     cv::namedWindow( "hsv image", cv::WINDOW_AUTOSIZE );
     cv::imshow( "hsv image", HSV );
@@ -83,25 +86,79 @@ void filterImage(cv::Mat originalImage){
 
 }
 
-cv::Mat originalImage;
-
 void myChoice( int event, int x, int y, int flags, void *userdata )
 {
     if( event == cv::EVENT_LBUTTONDOWN ) {
-        filterImage(originalImage);
+        //std::cout << "key: ";
+        //std::cout << CHOICE << std::endl;
+        //std::cout << "(char)key: ";
+        //std::cout << (char)CHOICE << std::endl;
+        /*
+        switch( CHOICE )
+        {
+        case '0': case '1': case '2': case '3': case 4: case 5: case 6: case 7: case 8: case 9: H_MIN = CHOICE; break;
+        default: H_MIN = 0; break;
+        }
+         */
+
+        if ((((char) PREVCHOICE == 'H') || ((char) PREVCHOICE == 'h')) &&
+            (((char) CHOICE == 'I') || ((char) CHOICE == 'i'))) {
+            H_MIN = num;
+        } else if ((((char) PREVCHOICE == 'H') || ((char) PREVCHOICE == 'h')) &&
+                   (((char) CHOICE == 'A') || ((char) CHOICE == 'a'))) {
+            H_MAX = num;
+        } else if ((((char) PREVCHOICE == 'S') || ((char) PREVCHOICE == 's')) &&
+                   (((char) CHOICE == 'I') || ((char) CHOICE == 'i'))) {
+            S_MIN = num;
+        } else if ((((char) PREVCHOICE == 'S') || ((char) PREVCHOICE == 's')) &&
+                   (((char) CHOICE == 'A') || ((char) CHOICE == 'a'))) {
+            S_MAX = num;
+        } else if ((((char) PREVCHOICE == 'V') || ((char) PREVCHOICE == 'v')) &&
+                   (((char) CHOICE == 'I') || ((char) CHOICE == 'i'))) {
+            V_MIN = num;
+        } else if ((((char) PREVCHOICE == 'V') || ((char) PREVCHOICE == 'v')) &&
+                   (((char) CHOICE == 'A') || ((char) CHOICE == 'a'))) {
+            V_MAX = num;
+        }
+
+        filterImage();
+        std::cout << "H_MIN: ";
+        std::cout << H_MIN;
+        std::cout << ", H_MAX: ";
+        std::cout << H_MAX;
+        std::cout << ", S_MIN: ";
+        std::cout << S_MIN;
+        std::cout << ", S_MAX: ";
+        std::cout << S_MAX;
+        std::cout << ", V_MIN: ";
+        std::cout << V_MIN;
+        std::cout << ", V_MAX: ";
+        std::cout << V_MAX;
+        std::cout << "num: ";
+        std::cout << num << std::endl;
     }
 }
 
-int CHOICE; int PREVCHOICE; int c; int d; int u; int num;
-int H_MIN = 0; int H_MAX = 256; int S_MIN = 0; int S_MAX = 256; int V_MIN = 0; int V_MAX = 256;
+int charToInt(char c){
+    int i;
+    switch(c)
+    {
+        case '0' : i = 0; break; case '1' : i = 1; break; case '2' : i = 2; break; case '3' : i = 3; break;
+        case '4' : i = 4; break; case '5' : i = 5; break; case '6' : i = 6; break; case '7' : i = 7; break;
+        case '8' : i = 8; break; case '9' : i = 9; break; default: i = 0; break;
+    }
+    return i;
+}
 
 // MAIN
 
 int main( int argc, char** argv ) {
+    std::string fn;
     if (argc != 2) {
         std::cout << "The name of the image file is missing !!" << std::endl;
-
-        return -1;
+        fn = "IMG_20191219_184809.jpg";
+    } else {
+        fn = argv[1];
     }
 
     std::cout << std::endl;
@@ -110,9 +167,11 @@ int main( int argc, char** argv ) {
 
     std::cout << std::endl;
 
-    originalImage = cv::imread(argv[1], cv::IMREAD_UNCHANGED);
-    resize(originalImage, originalImage,
-           cv::Size(originalImage.cols / 6, originalImage.rows / 6)); // to half size or even smaller
+    originalImage = cv::imread(fn, cv::IMREAD_UNCHANGED);
+    if (fn == "IMG_20191219_184809.jpg"){
+        resize(originalImage, originalImage,
+               cv::Size(originalImage.cols / 6, originalImage.rows / 6)); // to half size or even smaller
+    }
 
     if (originalImage.empty()) {
         // NOT SUCCESSFUL : the data attribute is empty
@@ -122,7 +181,8 @@ int main( int argc, char** argv ) {
         return -1;
     }
 
-    filterImage(originalImage);
+    H_MIN = 0; H_MAX = 256; S_MIN = 0; S_MAX = 256; V_MIN = 0; V_MAX = 256;
+    filterImage();
 
     // Create window
     cv::namedWindow("Original Image", cv::WINDOW_AUTOSIZE);
@@ -184,38 +244,45 @@ int main( int argc, char** argv ) {
     // Processing keyboard events
 
     for( ; ; ){
-        PREVCHOICE = cv::waitKey(0);
-        CHOICE = cv::waitKey(0);
-        c = cv::waitKey(0);
-        d = cv::waitKey(0);
-        u = cv::waitKey(0);
-        num = c * 100 + d * 10 + u;
 
-        if (((char) CHOICE == 'Q') || ((char) CHOICE == 'q')) {
+        std::cout << std::endl;
+        std::cout << "Q --- Quit" << std::endl;
+        std::cout << "hi -- H_MIN; ha -- H_MAX; si -- S_MIN; sa -- S_MAX; i -- V_MIN; va -- V_MAX; " << std::endl;
+        std::cout << "then write 3 digits for the value" << std::endl;
+
+        PREVCHOICE = cv::waitKeyEx(0);
+        std::cout << (char) PREVCHOICE;
+
+        if( ((char)PREVCHOICE == 'Q') || ((char)PREVCHOICE == 'q') )
+        {
             break;
         }
 
-        if ((((char) PREVCHOICE == 'H') || ((char) PREVCHOICE == 'h')) &&
-            (((char) CHOICE == 'I') || ((char) CHOICE == 'i'))) {
-            H_MIN = num;
-        } else if ((((char) PREVCHOICE == 'H') || ((char) PREVCHOICE == 'h')) &&
-                   (((char) CHOICE == 'A') || ((char) CHOICE == 'a'))) {
-            H_MAX = num;
-        } else if ((((char) PREVCHOICE == 'S') || ((char) PREVCHOICE == 's')) &&
-                   (((char) CHOICE == 'I') || ((char) CHOICE == 'i'))) {
-            S_MIN = num;
-        } else if ((((char) PREVCHOICE == 'S') || ((char) PREVCHOICE == 's')) &&
-                   (((char) CHOICE == 'A') || ((char) CHOICE == 'a'))) {
-            S_MAX = num;
-        } else if ((((char) PREVCHOICE == 'V') || ((char) PREVCHOICE == 'v')) &&
-                   (((char) CHOICE == 'I') || ((char) CHOICE == 'i'))) {
-            V_MIN = num;
-        } else if ((((char) PREVCHOICE == 'V') || ((char) PREVCHOICE == 'v')) &&
-                   (((char) CHOICE == 'A') || ((char) CHOICE == 'a'))) {
-            V_MAX = num;
+        CHOICE = cv::waitKeyEx(0);
+        std::cout << (char) CHOICE << std::endl;
+
+        if( ((char)CHOICE == 'Q') || ((char)CHOICE == 'q') )
+        {
+            break;
         }
 
+        int c_tmp; int d_tmp; int u_tmp;
+        c_tmp = cv::waitKeyEx(0);
+        c = charToInt((char)c_tmp);
+        std::cout << "c : " << c << std::endl;
 
+        d_tmp = cv::waitKeyEx(0);
+        d = charToInt((char)d_tmp);
+        std::cout << "d : " << d << std::endl;
+
+        u_tmp = cv::waitKeyEx(0);
+        u = charToInt((char)u_tmp);
+        std::cout << "u : " << u << std::endl;
+
+        num = c * 100 + d * 10 + u;
+        std::cout << "num : " << num << std::endl;
+
+        std::cout << "confirm with mouse LEFT BUTTON" << std::endl;
     }
     // Destroy the windows --- Actually not needed in such a simple program
 
