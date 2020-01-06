155b0a8f97df9fd3fecff88a0c60aeaea9200b63
Inês Justo <ines.zita7@gmail.com>
2020-01-06 01:38:00 +0000

    .

---
+++ build/CMakeFiles/proj_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proj_main.dir/main.cpp.o"
+  "proj_main.pdb"
+  "proj_main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proj_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
